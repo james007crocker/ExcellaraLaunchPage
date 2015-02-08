90263f370ce15798e02d482948ef139dedefa165
James Crocker <jcrocke5@uwo.ca>
2015-02-08 13:08:57 -0500

    Noticed Emial REcipient Was MIstyped

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
 	def welcome_email(email, role)
 		@email = email
 		@role = role
-		mail(to: "sign.up.excellara.com", subject:"Excellara Hit")
+		mail(to: "sign.up.excellara@gmail.com", subject:"Excellara Hit")
 	end
 end
