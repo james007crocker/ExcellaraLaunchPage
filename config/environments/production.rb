880635833633012b82ba4d9c8c85a8b6c57e4602
James Crocker <jcrocke5@uwo.ca>
2015-02-08 12:15:45 -0500

    Hide Private Info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,12 +1,18 @@
 Rails.application.configure do
-  
+  config.action_mailer.default_url_options = { :host => 'excellara.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'excellara.herokuapp.com'
+
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  #domain:               'gmail.com',
-  user_name:            'sign.up.excellara@gmail.com',
-  password:             'excellara123',
+  domain:               ENV['GMAL_DOMAIN'],
+  user_name:            ENV["GMIAL_USERNAME"],
+  password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true  }
 
