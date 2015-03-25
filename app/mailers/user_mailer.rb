c3436ff6b2d8b0bfe88a7881bafcc90fcbbf69af
James Crocker <jcrocke5@uwo.ca>
2015-03-24 22:19:19 -0400

    Replace Email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -15,6 +15,6 @@ class UserMailer < ApplicationMailer
 
   def pilot_email(email2)
     @email2 = email2
-    mail(to: "admin@excellara.com" , subject:"Pilot Interest")
+    mail(to: "lauren@excellara.com" , subject:"Pilot Interest")
   end
 end
